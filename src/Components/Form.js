416a885b2fa5f126fdebb858ec613efae1843934
dilshadbasith <dilshadbasith01@gmail.com>
2023-10-03 04:12:26 -0700

    next push

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,27 +1,25 @@
 import React from "react";
 import { addTodos } from "../redux/Reducers/TodoReducer";
-import {useDispatch} from 'react-redux'
+import { useDispatch } from "react-redux";
 
 function Form() {
-    const dispatch=useDispatch()
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        const value =e.target.add.value;
-        dispatch(addTodos(value))
-        e.target.reset()
-    }
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = e.target.add.value;
+    dispatch(addTodos(value));
+    e.target.reset();
+  };
 
   return (
     <div>
-      <form className="form-group custom-form" onSubmit={handleSubmit}>
+      <form className="form-group custom-form" onSubmit={handleSubmit} required>
         <label>Add your Todo Items</label>
         <div className="input-and-btn">
-          <input
-            type="text"
-            className="form-control1"
-            id="add"
-          />
-          <button type="submit" className="btn btn-secondary btn-md">Add</button>
+          <input type="text" className="form-control1" id="add" required />
+          <button type="submit" className="btn btn-secondary btn-md">
+            Add
+          </button>
         </div>
       </form>
     </div>
